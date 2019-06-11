6015c0d2b7157fc540760773323635bc514ddd99
In Young Choi <inkatsoy08@gmail.com>
2019-06-11 22:04:48 +0600

    add 2 pages

--- build/server.php
+++ build/server.php
@@ -15,11 +15,11 @@
         <body>
             <h2>Request</h2>
             <ul>
-                <li>Имя: ' . $name . '</li>
-                <li>Телефон: ' . $phone . '</li>
+                <li>Name: ' . $name . '</li>
+                <li>Phone number: ' . $phone . '</li>
                 <li>Email: ' . $mail . '</li>
-                <li>Тема: ' . $theme . '</li>
-                <li>Сообщение: ' . $comment . '</li>
+                <li>Theme: ' . $theme . '</li>
+                <li>Message: ' . $comment . '</li>
             </ul>
         </body>
     </html>    
